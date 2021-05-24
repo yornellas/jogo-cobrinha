2021829086d965dc68fcd02393cf2c98f422ee6b
Yolanda Ornellas <ymf.ornellas@icloud.com>
2021-05-24 17:50:21 -0300

    defines directions and sets looping

--- script.js
+++ script.js
@@ -23,7 +23,22 @@ function createSnake() {
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left"; //cada código corresponde a uma tecla do teclado
+    if (event.keyCode == 38 && direction != "down") direction = "up"; 
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+
+}
+
 function startGame() {
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0; //isso fará com que a cobrinha não saia da tela
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     createBackground();
     createSnake();
 
